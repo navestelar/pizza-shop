594627240d4e83fc1613aec73451333e26ae6de3
Ana Carolina Capistrano Huntemann <acch.escola@gmail.com>
2024-07-07 20:14:28 -0300

    Instalando shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import './global.css'
+import {Button} from "@/components/ui/button.tsx";
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Enviar</Button>
+    </div>
   )
 }
