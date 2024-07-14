fdba01cc53b8e6321979d3379ca9999d33063cd5
Ana Carolina Capistrano Huntemann <acch.escola@gmail.com>
2024-07-14 10:15:19 -0300

    Página: Cadastro de restaurante

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,6 +5,7 @@ import { SignIn } from '@/pages/auth/sign-in'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
   {
@@ -24,6 +25,10 @@ export const router = createBrowserRouter([
       {
         path: '/sign-in',
         element: <SignIn />,
+      },
+      {
+        path: '/sign-up',
+        element: <SignUp />,
       },
     ],
   },
