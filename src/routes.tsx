bf53a9d7af435222d1656f569ebd447367c6eeac
Ana Carolina Capistrano Huntemann <acch.escola@gmail.com>
2024-07-14 14:46:33 -0300

    Página: Dashboard

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import { Dashboard } from '@/pages/app/dashboard'
 import { SignIn } from '@/pages/auth/sign-in'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
+import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignUp } from './pages/auth/sign-up'
 
