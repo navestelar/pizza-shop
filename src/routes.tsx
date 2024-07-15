7469f17af61d8b23da0066bb37091dd297d65941
Ana Carolina Capistrano Huntemann <acch.escola@gmail.com>
2024-07-15 08:21:33 -0300

    Página 404 (Not Found)

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { SignIn } from '@/pages/auth/sign-in'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
+import { NotFound } from './pages/404'
 import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignUp } from './pages/auth/sign-up'
@@ -12,6 +13,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: '/',
