1e11af33b989df7cb0ed72c3181df95570589d5f
Ana Carolina Capistrano Huntemann <acch.escola@gmail.com>
2024-08-04 10:12:29 -0300

    Conexão com API - Filtros dos pedidos

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -3,6 +3,9 @@ import { api } from '@/lib/axios'
 
 export interface GetOrdersQuery {
   pageIndex?: number | null
+  orderId?: string | null
+  customerName?: string | null
+  status?: string | null
 }
 
 export interface Order {
@@ -22,10 +25,18 @@ export interface getOrdersResponse {
   }
 }
 
-export async function getOrders({ pageIndex }: GetOrdersQuery) {
+export async function getOrders({
+  pageIndex,
+  orderId,
+  customerName,
+  status,
+}: GetOrdersQuery) {
   const response = await api.get<getOrdersResponse>('/orders', {
     params: {
       pageIndex,
+      orderId,
+      customerName,
+      status,
     },
   })
 
