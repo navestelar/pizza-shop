a0a008451281145f5d3e0ad57871c013685be71b
Ana Carolina Capistrano Huntemann <acch.escola@gmail.com>
2024-08-04 16:23:53 -0300

    Conexão com API - Receita no período

--- src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
+++ src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
@@ -27,11 +27,11 @@ export function MonthCanceledOrdersAmountCard() {
             <p className="text-xs text-muted-foreground">
               {monthCanceledOrdersAmount.diffFromLastMonth <= 0 ? (
                 <span className="text-emerald-500 dark:text-emerald-400">
-                  -{monthCanceledOrdersAmount.diffFromLastMonth}%
+                  {monthCanceledOrdersAmount.diffFromLastMonth}%
                 </span>
               ) : (
                 <span className="text-rose-500 dark:text-rose-400">
-                  {monthCanceledOrdersAmount.diffFromLastMonth}%
+                  +{monthCanceledOrdersAmount.diffFromLastMonth}%
                 </span>
               )}{' '}
               em relação ao mês passado
