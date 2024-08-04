b6b2af1540c2d4561be1c3aed2caf5053612a3a0
Ana Carolina Capistrano Huntemann <acch.escola@gmail.com>
2024-08-04 17:13:22 -0300

    Loading & Empty states - Erros e Página 404

--- src/routes.tsx
+++ src/routes.tsx
@@ -8,12 +8,13 @@ import { NotFound } from './pages/404'
 import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignUp } from './pages/auth/sign-up'
+import { Error } from './pages/error'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       {
         path: '/',
@@ -38,5 +39,9 @@ export const router = createBrowserRouter([
         element: <SignUp />,
       },
     ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ])
